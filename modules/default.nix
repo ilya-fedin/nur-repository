723361d58c2b9eec2a033bafe70dfa8c9a453e85
Ilya Fedin <fedin-ilja2010@ya.ru>
2019-12-19 23:49:46 +0400

    add iris-driver module

--- modules/default.nix
+++ modules/default.nix
@@ -2,4 +2,5 @@
   dbus-broker = import ./dbus-broker;
   default-emoji-font = import ./default-emoji-font;
   metric-compatible-fonts = import ./metric-compatible-fonts;
+  iris-driver = import ./iris-driver;
 }
