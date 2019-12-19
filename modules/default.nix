babb420b0e01a127333bbd171e8e8da99c86e66f
Ilya Fedin <fedin-ilja2010@ya.ru>
2019-12-20 00:56:00 +0400

    add cascadia-code-fallback module

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@
   dbus-broker = import ./dbus-broker;
   default-emoji-font = import ./default-emoji-font;
   metric-compatible-fonts = import ./metric-compatible-fonts;
+  cascadia-code-fallback = import ./cascadia-code-fallback;
   iris-driver = import ./iris-driver;
   io-scheduler = import ./io-scheduler;
 }
