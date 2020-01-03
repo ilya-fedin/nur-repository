36e607e352f923d5692cba283fff677a46dc0cb1
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-01-04 02:25:55 +0400

    Update kotatogram-desktop to k1.1.2

--- default.nix
+++ default.nix
@@ -8,7 +8,8 @@ rec {
   cmake_3_16 = callPackage ./pkgs/cmake {};
 
   kotatogram-desktop = qt5.callPackage ./pkgs/kotatogram-desktop {
-    inherit libtgvoip rlottie-tdesktop;
+    cmake = cmake_3_16;
+    inherit rlottie-tdesktop;
   };
 
   libtgvoip = callPackage ./pkgs/libtgvoip {};
