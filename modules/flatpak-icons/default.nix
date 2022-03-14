69a2aa9ab850342949ff18edaeef219d78d59c95
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-03-14 17:39:46 +0400

    flatpak-icons: environment.icons -> xdg.icons.icons

--- modules/flatpak-icons/default.nix
+++ modules/flatpak-icons/default.nix
@@ -2,33 +2,37 @@
 
 with lib;
 let
+  cfg = config.xdg.icons;
+
   iconDir = pkgs.runCommand "icons" { preferLocalBuild = true; } ''
     mkdir -p "$out"
     ${concatMapStrings (p: ''
         if [ -d "${p}/share/icons" ]; then
             find -L "${p}/share/icons" -mindepth 1 -maxdepth 1 -type d -exec cp -rn --no-preserve=mode,ownership {}/ "$out" \;
         fi
-    '') config.environment.icons}
+    '') cfg.icons}
   '';
 in {
   options = {
-    environment = {
-      icons = mkOption {
-        type = types.listOf types.package;
-        default = [];
-        example = literalExpression "[ pkgs.papirus-icon-theme pkgs.breeze-icons ]";
-        description = ''
-          The set of packages that appear in
-          /usr/share/icons. These packages are
-          automatically available to all users, and are
-          automatically updated every time you rebuild the system
-          configuration.
-        '';
+    xdg = {
+      icons = {
+        icons = mkOption {
+          type = types.listOf types.package;
+          default = [];
+          example = literalExpression "[ pkgs.papirus-icon-theme pkgs.breeze-icons ]";
+          description = ''
+            The set of packages that appear in
+            /usr/share/icons. These packages are
+            automatically available to all users, and are
+            automatically updated every time you rebuild the system
+            configuration.
+          '';
+        };
       };
     };
   };
 
-  config = mkIf (config.environment.icons != []) {
+  config = mkIf (cfg.enable && cfg.icons != []) {
       fileSystems."/usr/share/icons" = {
         device = "${iconDir}";
         fsType = "none";
