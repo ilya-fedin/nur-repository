fb0bfca52f8d1d4b4c47bfb9c522b3049c66f5ed
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-03-14 16:58:07 +0400

    Create a separate config option for icons

--- modules/flatpak-icons/default.nix
+++ modules/flatpak-icons/default.nix
@@ -8,14 +8,32 @@ let
         if [ -d "${p}/share/icons" ]; then
             find -L "${p}/share/icons" -mindepth 1 -maxdepth 1 -type d -exec cp -rn --no-preserve=mode,ownership {}/ "$out" \;
         fi
-    '') config.environment.systemPackages}
+    '') config.environment.icons}
   '';
 in {
-  config = {
+  options = {
+    environment = {
+      icons = mkOption {
+        type = types.listOf types.package;
+        default = [];
+        example = literalExpression "[ pkgs.papirus-icon-theme pkgs.breeze-icons ]";
+        description = ''
+          The set of packages that appear in
+          /usr/share/icons. These packages are
+          automatically available to all users, and are
+          automatically updated every time you rebuild the system
+          configuration.
+        '';
+      };
+    };
+  };
+
+  config = mkIf (config.environment.icons != []) {
       fileSystems."/usr/share/icons" = {
         device = "${iconDir}";
         fsType = "none";
         options = [ "bind" ];
       };
+      environment.sessionVariables.XDG_DATA_DIRS = [ "/usr/share" ];
   };
 }
