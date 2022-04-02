585f25f0fee5854b27906e20ef9525fc32274771
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-03 03:14:52 +0400

    indicator overlay: switch to ayatana-indicator

--- overlays/indicator/default.nix
+++ overlays/indicator/default.nix
@@ -24,17 +24,20 @@ self: super: {
     ];
 
     mate-indicator-applet = super.mate.mate-indicator-applet.overrideAttrs(oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ (with self; [ ayatana-ido libayatana-indicator-gtk3 ]);
       postPatch = ''
         substituteInPlace src/applet-main.c \
           --replace '/usr' '/run/current-system/sw'
       '';
+
+      configureFlags = [ "--with-ayatana-indicators" ];
     });
   };
 
-  libindicator-gtk3 = super.libindicator-gtk3.overrideAttrs(oldAttrs: {
-    postPatch = oldAttrs.postPatch + ''
-      substituteInPlace libindicator/indicator3-0.4.pc.in.in \
-        --replace 'indicatordir=''${libdir}' 'indicatordir=/run/current-system/sw/lib'
+  libayatana-indicator-gtk3 = super.libayatana-indicator-gtk3.overrideAttrs(oldAttrs: {
+    postPatch = ''
+      substituteInPlace libayatana-indicator/ayatana-indicator3-0.4.pc.in.in \
+        --replace 'indicatordir=@libdir@' 'indicatordir=/run/current-system/sw/lib'
     '';
   });
 
