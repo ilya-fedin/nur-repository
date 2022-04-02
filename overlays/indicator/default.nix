678628e9be629ef31e5de4f5cd7ff1a1cb6dce12
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-03 02:51:11 +0400

    indicator overlay: fix mate-indicator-applet

--- overlays/indicator/default.nix
+++ overlays/indicator/default.nix
@@ -1,4 +1,36 @@
 self: super: {
+  mate = super.mate // {
+    extraPackages = with self.mate; [
+      atril
+      caja-extensions
+      engrampa
+      eom
+      mate-applets
+      mate-backgrounds
+      mate-calc
+      mate-indicator-applet
+      mate-media
+      mate-netbook
+      mate-power-manager
+      mate-screensaver
+      mate-sensors-applet
+      mate-system-monitor
+      mate-terminal
+      mate-user-guide
+      # mate-user-share
+      mate-utils
+      mozo
+      pluma
+    ];
+
+    mate-indicator-applet = super.mate.mate-indicator-applet.overrideAttrs(oldAttrs: {
+      postPatch = ''
+        substituteInPlace src/applet-main.c \
+          --replace '/usr' '/run/current-system/sw'
+      '';
+    });
+  };
+
   libindicator-gtk3 = super.libindicator-gtk3.overrideAttrs(oldAttrs: {
     postPatch = oldAttrs.postPatch + ''
       substituteInPlace libindicator/indicator3-0.4.pc.in.in \
