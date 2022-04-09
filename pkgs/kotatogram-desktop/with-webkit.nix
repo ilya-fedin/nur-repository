f74c52045de6f7f20305bd1e1079d769705054a1
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-10 01:44:25 +0400

    kotatogram-desktop-with-webkit: only for linux

--- pkgs/kotatogram-desktop/with-webkit.nix
+++ pkgs/kotatogram-desktop/with-webkit.nix
@@ -1,5 +1,7 @@
 { stdenv, lib, kotatogram-desktop, glib-networking, webkitgtk, makeWrapper }:
 
+with lib;
+
 stdenv.mkDerivation {
   pname = "${kotatogram-desktop.pname}-with-webkit";
   version = kotatogram-desktop.version;
@@ -13,7 +15,9 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     makeWrapper ${kotatogram-desktop}/bin/kotatogram-desktop $out/bin/kotatogram-desktop \
       --prefix GIO_EXTRA_MODULES : ${glib-networking}/lib/gio/modules \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ webkitgtk ]}
+      --prefix LD_LIBRARY_PATH : ${makeLibraryPath [ webkitgtk ]}
   '';
-  meta = kotatogram-desktop.meta;
+  meta = kotatogram-desktop.meta // {
+    platforms = platforms.linux;
+  };
 }
