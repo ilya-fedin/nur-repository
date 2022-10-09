4d3eb2f06912123606d27f5875e8531692c69e39
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-10-09 04:23:10 +0400

    Revert "Try to fix restricted repository evaluation"
    This reverts commit 324149ca4c32062a26ed812a64c2a2b1e2384669.

--- flake-compat.nix
+++ flake-compat.nix
@@ -1,7 +1,7 @@
 (import
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    (import <nixpkgs> {}).fetchzip {
+    fetchTarball {
       url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }
