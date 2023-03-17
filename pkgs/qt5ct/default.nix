2cc523136c253a8a9277a5bcdb1162d5ad9b315f
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-03-18 01:46:41 +0400

    qt5ct: update to 1.7

--- pkgs/qt5ct/default.nix
+++ pkgs/qt5ct/default.nix
@@ -1,4 +1,11 @@
 pkgs: with pkgs; with libsForQt5; qt5ct.overrideAttrs(oldAttrs: rec {
+  version = "1.7";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/${oldAttrs.pname}/${oldAttrs.pname}-${version}.tar.bz2";
+    sha256 = "sha256-7VhUam5MUN/tG5/2oUjUpGj+m017WycnuWUB3ilVuNc=";
+  };
+
   buildInputs = oldAttrs.buildInputs ++ ([
     qtquickcontrols2 kconfig kconfigwidgets kiconthemes
   ]);
