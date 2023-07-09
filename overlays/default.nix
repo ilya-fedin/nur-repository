f049002e3790cf091903b4016de1e71819b3ba92
name_snrl <Demogorgon-74@ya.ru>
2023-07-09 11:36:13 +0500

    add default overlay that contains custom pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,4 +3,10 @@
   mate-wayland = import ./mate-wayland;
   portal = import ./portal;
   qt5ct = import ./qt5ct;
+  default = self: super: super.lib.filterAttrs
+    (name: _: name != "modules" && name != "overlays")
+    (import ../. {
+      pkgs = super;
+      inputs = (import ../flake-compat.nix).inputs;
+    });
 }
