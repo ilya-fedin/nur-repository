53a2f07244f62943839e78377481aff6adb6f792
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-03-28 05:30:24 +0400

    qt5ct: remove version override

--- pkgs/qt5ct/default.nix
+++ pkgs/qt5ct/default.nix
@@ -1,11 +1,4 @@
 pkgs: with pkgs; with libsForQt5; qt5ct.overrideAttrs(oldAttrs: rec {
-  version = "1.8";
-
-  src = fetchurl {
-    url = "mirror://sourceforge/${oldAttrs.pname}/${oldAttrs.pname}-${version}.tar.bz2";
-    sha256 = "sha256-I7dAVEFepBJDKHcu+ab5UIOpuGVp4SgDSj/3XfrYCOk=";
-  };
-
   buildInputs = oldAttrs.buildInputs ++ ([
     qtquickcontrols2 kconfig kconfigwidgets kiconthemes
   ]);
