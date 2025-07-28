f97456b1d131fb46dddeecb0eb17dbd907cdac65
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-07-29 00:09:02 +0400

    Remove vscode module
    nixpkgs has one now

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,6 @@
   io-scheduler = import ./io-scheduler;
   metric-compatible-fonts = import ./metric-compatible-fonts;
   noto-cjk = import ./noto-cjk;
-  vscode = import ./vscode;
   qt5ct = import ./qt5ct;
   qt6ct = import ./qt6ct;
   unified-kernel-image = import ./unified-kernel-image;
