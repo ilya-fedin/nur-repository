f663879d609b20261a01e463e3ff8dbbe2426272
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-11-27 16:45:16 +0400

    airsane: init

---
+++ pkgs/airsane/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  ninja,
+  coreutils,
+  sane-backends,
+  avahi,
+  libusb1,
+  libjpeg,
+  libpng,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "airsane";
+  version = "0.4.6";
+
+  src = fetchFromGitHub {
+    owner = "SimulPiscator";
+    repo = "AirSane";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-U2FrcoLMZ9WmAkqMYnPOOV2JRkMUVowqzyfr2/NKbcU=";
+  };
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+        --replace-fail ' /etc/airsane' ' ''${CMAKE_INSTALL_PREFIX}/etc/airsane' \
+        --replace-fail 'DESTINATION /' 'DESTINATION '
+    substituteInPlace systemd/airsaned.service.in \
+        --replace-fail '=/bin' '=${coreutils}/bin' \
+        --replace-fail '/usr/bin/scanimage' '${sane-backends}/bin/scanimage'
+  '';
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+  ];
+
+  buildInputs = [
+    sane-backends
+    avahi
+    libusb1
+    libjpeg
+    libpng
+  ];
+
+  meta = {
+    description = "Publish SANE scanners to MacOS, Android, and Windows via Apple AirScan";
+    homepage = "https://github.com/SimulPiscator/AirSane";
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.unix;
+  };
+})
